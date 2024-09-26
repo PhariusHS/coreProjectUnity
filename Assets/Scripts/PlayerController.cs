27ecb901d54f0cb3ac042ecf18fe0ad0c2566948
Luca Ponteprimo <Lucaponteprimo04@gmail.com>
2024-09-26 22:46:16 +0200

    Jumping improvement(NotFinished)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigidBody2D;
-    private CircleCollider2D circleCollider2D;
+    private BoxCollider2D boxCollider2D;
     private bool isFacingRight = true;
     [SerializeField] private LayerMask groundLayer;
     [Range(0, 60f)] [SerializeField] private float speed = 25;
     [Range(0, 5f)][SerializeField] private float fallLongMult = 0.85f;
     [Range(0, 5f)][SerializeField] private float fallShortMult = 1.55f;
+    [Range(0, 12f)] [SerializeField] private float jumpVelocity = 7f;
+
     float moveX = 0f;
     bool jump = false, jumpHeld = false;
 
-    
+
 
     //Is the character touching ground?
-    private bool isGrounded()
+    private bool IsGrounded()
     {
-        RaycastHit2D hit = Physics2D.CircleCast(circleCollider2D.bounds.center, circleCollider2D.radius, Vector2.down, 0.1f, groundLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
         return hit.collider != null;
     }
 
@@ -29,7 +31,7 @@ public class PlayerController : MonoBehaviour
     {
         
         rigidBody2D = GetComponent<Rigidbody2D>();
-        circleCollider2D = GetComponent<CircleCollider2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
 
     }
 
@@ -40,17 +42,15 @@ public class PlayerController : MonoBehaviour
         moveX = Input.GetAxisRaw("Horizontal") * speed;
 
         //Getting right animation while moving
-        
-        Console.WriteLine(moveX);
+        if (IsGrounded() && moveX.Equals(0))
+            GetComponent<Animator>().Play("PlayerIdle");
+        else if (IsGrounded() && (moveX > 0 || moveX < 0))
+            GetComponent<Animator>().Play("PlayerWalk");
 
-        if (!moveX.Equals(0))
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
         {
-            if (moveX is > 0 or < 0)
-                GetComponent<Animator>().Play("PlayerWalk");
+            jump = true;
         }
-        else GetComponent<Animator>().Play("PlayerIdle");
-
-
 
     }
 
@@ -66,6 +66,15 @@ public class PlayerController : MonoBehaviour
         //Flipping the sprite -> Calling flipSprite()
         if (moveFactor > 0f && !isFacingRight) flipSprite();
         else if (moveFactor < 0 && isFacingRight) flipSprite();
+
+
+        //Jumping
+       if(jump)
+        {
+
+            rigidBody2D.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse); // Añade un impulso para saltar
+            jump = false;
+        }
     }
 
 
